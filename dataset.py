b10a3ef5ea133034c4551c806723d2481f608794
Matthew Cooper <mscoop@umich.edu>
2023-11-27 14:13:19 -0500

    Added baseline dataset reading implementation

---
+++ dataset.py
@@ -0,0 +1,26 @@
+"""
+
+File for loading in Spotify Million Songs Dataset
+
+"""
+
+import os
+import pandas as pd
+import numpy as np
+
+class SpotifyDataset:
+
+    def __init__(self, file_path):
+        """Read in the necessary data from the specified filepath"""
+
+        self.df = pd.read_csv(file_path)
+    
+    def __len__(self):
+        """Returns size of the dataset"""
+
+        return len(self.df)
+    
+    def get_lyrics(self, index):
+        """Returns the lyrics at a specific index"""
+
+        return self.df.loc[index, 'text']
